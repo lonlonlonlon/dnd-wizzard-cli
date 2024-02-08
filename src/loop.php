113b0981b48bfbd7bf257406c5776bbe8db1dccf
lol <olbrueck.l99@gmail.com>
2024-02-08 20:23:39 +0100

    init

--- src/loop.php
+++ src/loop.php
@@ -2,18 +2,19 @@
 
 
 function mainLoop () {
-    $cols = shell_exec('tput cols') - 1;
-    $rows = shell_exec('tput lines') - 1;
+    $cols = system('tput cols') - 1;
+    $lines = system('tput lines') - 1;
     while (1) {
         $displayString = TERM_BACK_PURPLE_DARK;
-        for ($x = 0; $x < $cols; $x++) {
+        for ($x = 0; $x < $lines; $x++) {
             for ($y = 0; $y < $cols; $y++) {
                 $displayString .= ' ';
             }
             $displayString .= [TERM_BACK_CYAN_LIGHT, TERM_BACK_GREEN_LIGHT, TERM_BACK_YELLOW_LIGHT][rand(0,2)].PHP_EOL;
+            echo ($displayString);
+            $displayString = '';
         }
+        echo(TERM_RESET);
         system('clear');
-        echo($displayString);
-        sleep(1);
     }
 }
